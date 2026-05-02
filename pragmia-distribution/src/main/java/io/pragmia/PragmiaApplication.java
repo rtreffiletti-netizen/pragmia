341144195f5a4429292bae9239e45c653584900d
rtreffiletti-netizen <r.treffiletti@gmail.com>
2026-05-02 13:00:40 +0200

    fix: aggiunto @EnableJpaRepositories + @EntityScan per fat JAR multi-modulo

--- pragmia-distribution/src/main/java/io/pragmia/PragmiaApplication.java
+++ pragmia-distribution/src/main/java/io/pragmia/PragmiaApplication.java
@@ -2,9 +2,13 @@ package io.pragmia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.pragmia")
+@EnableJpaRepositories(basePackages = "io.pragmia")
+@EntityScan(basePackages = "io.pragmia")
 @EnableScheduling
 public class PragmiaApplication {
     public static void main(String[] args) {
